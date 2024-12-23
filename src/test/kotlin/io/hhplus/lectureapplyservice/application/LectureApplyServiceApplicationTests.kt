9d94ec3b51d6ec6467e24468597e5ed578b1e9ca
ydj515 <ydj515@hanmail.net>
2024-12-24 03:10:24 +0900

    chore: 오탈자 수정

--- src/test/kotlin/io/hhplus/lectureapplyservice/application/LectureApplyServiceApplicationTests.kt
+++ src/test/kotlin/io/hhplus/lectureapplyservice/application/LectureApplyServiceApplicationTests.kt
@@ -3,9 +3,11 @@ package io.hhplus.lectureapplyservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.utility.TestcontainersConfiguration
 
 @Import(TestcontainersConfiguration::class)
+@ActiveProfiles("integration-test")
 @SpringBootTest
 class LectureApplyServiceApplicationTests {
     @Test
